682c5d687317cceda40542160015c34df811bf86
540904718 <540904718@qq.com>
2020-02-18 12:19:09 +0800

    注册模块-列偏移整合

--- src/views/login/components/registerDialog.vue
+++ src/views/login/components/registerDialog.vue
@@ -16,20 +16,20 @@
         </el-form-item>
         <el-form-item label="图形码" :label-width="formLabelWidth">
             <el-row>
-                <el-col :span="17">
+                <el-col :span="16">
                      <el-input v-model="form.name" autocomplete="off"></el-input>
                 </el-col>
-                <el-col class="regist-box" :span="7">
+                <el-col class="regist-box" :offset="1" :span="7">
                     <img class="regist-code" src="../../../assets/login_captcha.png" alt="">
                 </el-col>
             </el-row>
         </el-form-item>
         <el-form-item label="验证码" :label-width="formLabelWidth">
             <el-row>
-                <el-col :span="17">
+                <el-col :span="16">
                        <el-input v-model="form.name" autocomplete="off"></el-input>
                 </el-col>
-                <el-col :span="7">
+                <el-col :offset="1" :span="7">
                     <el-button>获取用户验证码</el-button>
                 </el-col>
             </el-row>
