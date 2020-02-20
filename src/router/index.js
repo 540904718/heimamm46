bc27bf406d1629eb91b5d4cee0a1d047255e189f
540904718 <540904718@qq.com>
2020-02-21 00:19:13 +0800

    首页模块 - 整体布局

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,26 @@ import Vue from 'vue'
 import VueRouter from '../../node_modules/vue-router'
 // 导入 组件 登录
 import login from '../views/login/login.vue'
+
+import index from '../views/index/index.vue'
 // 注册一下 use
 Vue.use(VueRouter)
 // 创建路由对象
 const router = new VueRouter({
     // 路由规则
+    // 登录
     routes: [
         {
             path: '/login',
             component: login
+        },
+
+        {
+            path: '/index',
+            component: index
         }
+
+
     ]
 })
 // 暴露出去
---
+++ src/views/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-container class="my-container">
+    <el-header class="my-header">Header</el-header>
+    <el-container>
+      <el-aside class="my-aside" width="200px">Aside</el-aside>
+      <el-main class="my-main">Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+    .my-container {
+        height: 100%;
+        .my-header {
+            background-color: pink
+        }
+
+        .my-aside {
+            background-color: skyblue
+        }
+
+        .my-main {
+            background-color: gold
+        }
+    }
+</style>
